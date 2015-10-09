e4b0d2e5760846ce07696843a6f600b4bf6b5759
blueberryt <supikateriburu@gmail.com>
2015-10-09 22:30:35 +0900

    :lipstick: Add .gitignore and gem dotenv 準備万端！

--- dfg2c.rb
+++ dfg2c.rb
@@ -1,5 +1,27 @@
-require 'octokit'
-require 'json'
+require "json"
+require "dotenv"
+require "octokit"
 
-user = Octokit.user 'blueberryt'
-p user.repositories
+def see_childs(n)
+  case n
+  when Array
+    n.map{|x| see_childs(x) }
+  else
+    n.each{|x|}
+  end
+end
+
+def apply_env(val, val2 = nil)
+     val2 ||= self.class
+ case val
+  #when String
+   #val.gsub(/env\[(\w+?)\]/) { ENV[Regexp.last_match[1]] }
+ when Array
+   val.map { |v| apply_env(v, val2) }
+ when val2
+   val.each { |k, v| val[k] = apply_env(v, val2 ) }
+   val.to_h.to_json
+ else
+   val
+ end
+end
